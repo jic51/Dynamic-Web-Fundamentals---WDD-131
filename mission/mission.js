0e0c33a870fbbeb33655c1e81a9f63a95b0f5f21
jic51 <jicastro91@byui.edu>
2025-09-26 14:51:19 -0600

    changes made in html, js, and css

--- mission/mission.js
+++ mission/mission.js
@@ -5,7 +5,7 @@ const logo = document.querySelector('.logo');
 function changeTheme() {
   if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.src = 'byui-logo_white.webp'; // make sure this image exists in your project
+    logo.src = 'byui-logo_white.webp';
   } else {
     body.classList.remove('dark');
     logo.src = 'byui-logo_blue.webp';
